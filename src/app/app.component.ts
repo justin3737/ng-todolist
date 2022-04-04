c558c73a74f81160e089095e7d41874fe9aca408
Wu Justin <justin.cathay@gmail.com>
2022-04-04 21:14:59 +0800

    finished todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   header = 'Todo List';
-  items = [
-    {
-      id: 0,
-      name: 'Item 1',
-    },
-    {
-      id: 1,
-      name: 'Item 2',
-    }
-  ]
+  list:any[]=[];
+
+  addItem(item:string){
+    this.list.push({
+      id: uuidv4(),
+      name: item
+    });
+  }
+
+  removeItem(id:number){
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
