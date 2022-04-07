40a41aba4303d3d565d3f07c1a05c8603c3e9517
Wu Justin <justin.cathay@gmail.com>
2022-04-07 13:01:57 +0800

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { List } from '../app/model/list';
 import { v4 as uuidv4 } from 'uuid';
+import { TaskStatus } from '../app/enum/enum';
 
 @Component({
   selector: 'app-root',
@@ -11,28 +13,32 @@ export class AppComponent {
       .querySelectorAll<HTMLElement>('.def-btn')
       .forEach(node => node.click())
   }
-  list:any[]=[
-    {
-      id: uuidv4(),
-      name: 'Learn Angular',
-      isFinished: false
-    },
-    {
-      id: uuidv4(),
-      name: 'Learn Angular, Learn Angular, Learn Angular, Learn Angular, Learn Angular, Learn Angular',
-      isFinished: false
-    }
-  ];
 
-  addItem(item:string){
+  list : List[];
+
+  constructor(){
+    this.list = [
+      {
+        id: uuidv4(),
+        title: 'Learn Angular',
+        status: TaskStatus.Active
+      },
+      {
+        id: uuidv4(),
+        title: 'Learn Angular, Learn Angular, Learn Angular, Learn Angular, Learn Angular, Learn Angular',
+        status: TaskStatus.Active
+    }];
+  }
+
+  addItem(item:String){
     this.list.push({
       id: uuidv4(),
-      name: item,
-      isFinished: false
+      title: item,
+       status: TaskStatus.Active
     });
   }
 
-  removeItem(id:number){
-    this.list = this.list.filter(item => item.id !== id);
-  }
+  // removeItem(id:number){
+  //   this.list = this.list.filter(item => item.id !== id);
+  // }
 }
---
+++ src/app/model/list.ts
@@ -0,0 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
+import { TaskStatus } from '../enum/enum';
+
+export class List{
+    title : String;
+    status : TaskStatus;
+    id;
+    constructor(title: String){
+        this.title = title;
+        this.status = TaskStatus.Active;
+        this.id = uuidv4();
+    }
+}
